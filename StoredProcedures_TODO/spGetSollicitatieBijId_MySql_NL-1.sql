5cab79e8d9498618564e9bda66c47f67820e7e0e
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-15 13:11:32 +0100

    initial start

---
+++ StoredProcedures_TODO/spGetSollicitatieBijId_MySql_NL-1.sql
@@ -0,0 +1,67 @@
+
+	-- Step : 01.1
+	/******************************************************************************
+	-- Doel : Maak een nieuwe stored procedure aan heet spGetSollicitatieBijId.
+	-- ****************************************************************************
+	-- Versie     Datum          Auteur			Omschrijving
+	-- ******     **********     *******		**************
+	-- 01         24-11-2022     JamilM			Nieuw 
+	*******************************************************************************/ 
+    USE Sollicitatie;
+    DROP PROCEDURE IF EXISTS spGetSollicitatieBijId;
+    
+    DELIMITER //
+    
+	CREATE PROCEDURE spGetSollicitatieBijId
+    (
+    	id INT UNSIGNED
+    )
+	BEGIN
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+            SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+        END;
+         
+		START TRANSACTION;	
+			SELECT	 	 SOLL.Id									AS Id
+						,SOLL.Voornaam								AS Voornaam
+						,SOLL.Achternaam							AS Achternaam
+						,SOLL.Sollicitantnummer						AS Sollicitantnummer
+						,BEDR.Bedrijfnaam							AS Bedrijfnaam
+						,BEDR.Bedrijfcode							AS Bedrijfcode
+						,BEDR.Straat								AS Straat
+						,BEDR.Huisnummer							AS Huisnummer
+						,BEDR.Toevoeging							AS Toevoeging
+						,BEDR.Postcode								AS Postcode
+						,BEDR.Plaats								AS Plaats
+						,SOGE.Gespreksdatum							AS Gespreksdatum	
+						-- ,CONVERT(TIME(0), [SOGE].[Gesprekstijd])	AS Gesprekstijd
+						,SOGE.Gesprekstijd							AS Gesprekstijd
+						,SOGE.IsActief								AS IsActief
+						,SOGE.Opmerking								AS Opmerking		
+	
+			FROM	 	Sollicitant AS SOLL
+	
+			INNER JOIN 	Sollicitatiegesprek AS SOGE
+					ON 	SOGE.SollicitantId = SOLL.Id
+	
+			INNER JOIN 	Bedrijf AS BEDR
+					ON 	BEDR.Id = SOGE.BedrijfId
+					
+			WHERE		SOLL.Id = id;
+			
+			COMMIT;	
+                
+	END //
+    DELIMITER ;
+    
+/*****************************************Debug SP*****************************************
+    
+	CALL spGetSollicitatieBijId(1)
+	
+********************************************************************************************/
+
+   
+   
+   
