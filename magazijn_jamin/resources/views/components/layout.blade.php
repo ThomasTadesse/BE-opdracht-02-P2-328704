b147a65420884ccb0f8d7d1fdba6f880762cefe5
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-15 13:55:07 +0100

    home page initial start

---
+++ magazijn_jamin/resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L11</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body class="h-full">
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      {{ $slot }}
+    </div>
+  </main>
+</body>
+</html>
