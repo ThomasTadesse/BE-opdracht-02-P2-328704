0f658bc3bdd32e5a82f98006d084de961dea3fa4
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-19 11:49:52 +0100

    minor fixes

--- magazijn_jamin/resources/views/components/layout.blade.php
+++ magazijn_jamin/resources/views/components/layout.blade.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magazijn Jamin</title>
+    <link rel="stylesheet" href="resources/css/app.css">
     @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
     <div class="h-full flex flex-col">
-        <header class="bg-gray-800 text-white p-4">
-            <h1 class="text-2xl"> </h1>
-        </header>
         <main class="flex-1 p-4">
             {{ $slot }}
         </main>
--- magazijn_jamin/resources/views/welcome.blade.php
+++ magazijn_jamin/resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
-
-<p>Welcome to the home page!</p>
-
-
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl mb-8">
+            Welkom bij Magazijn Jamin
+        </h1>
+    </div>
 </x-layout>
