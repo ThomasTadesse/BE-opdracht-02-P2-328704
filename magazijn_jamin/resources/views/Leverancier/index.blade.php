3d77497c592d3851941094a877946f3d8c664fd6
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-29 16:11:10 +0100

    styling improvement

--- magazijn_jamin/resources/views/Leverancier/index.blade.php
+++ magazijn_jamin/resources/views/Leverancier/index.blade.php
@@ -1,10 +1,8 @@
 <x-layout>
-<div class="container mx-auto p-4">
-
-
-<h1 class="text-2xl font-bold mb-4 underline">Overzicht Leveranciers</h1>
-    <table class="min-w-full bg-white rounded-lg shadow-lg">
-        <thead>
+<div class="container mx-auto px-6 py-8">
+<h1 class="text-3xl font-semibold mb-6 underline">Overzicht Leveranciers</h1>
+    <table class="min-w-full table-auto border-collapse">
+        <thead class="bg-indigo-500 text-white">
             <tr>
                 <th class="py-2 px-4 text-sm font-medium text-gray-900">Naam</th>
                 <th class="py-2 px-4 text-sm font-medium text-gray-900">Contactpersoon</th>
@@ -17,14 +15,14 @@
         <tbody>
             @if(isset($leveranciers) && count($leveranciers) > 0)
                 @foreach ($leveranciers as $leverancier)
-                <tr>
+                <tr class="border-b border-gray-200 hover:bg-gray-50">
                     <td class="py-2 px-4 text-sm text-gray-900">{{ $leverancier->Naam }}</td>
                     <td class="py-2 px-4 text-sm text-gray-900">{{ $leverancier->Contactpersoon }}</td>
                     <td class="py-2 px-4 text-sm text-gray-900">{{ $leverancier->Leveranciernummer }}</td>
                     <td class="py-2 px-4 text-sm text-gray-900">{{ $leverancier->Mobiel }}</td>
                     <td class="py-2 px-4 text-sm text-gray-900">{{ $leverancier->unieke_producten_count }}</td>
-                    <td class="px-4 py-2 text-white rounded">
-                        <a href="{{ route('leverancier.show', $leverancier->id) }}" class="btn btn-primary">
+                    <td class="px-4 py-2 text-white">
+                        <a href="{{ route('leverancier.show', $leverancier->id) }}" class="text-indigo-600 hover:text-indigo-800">
                             📦
                         </a>
                     </td>
@@ -37,6 +35,7 @@
             @endif
         </tbody>
     </table>
+    <br>
     <a href="{{ route('welcome') }}" class="mb-4 inline-block px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Home</a>
 </div>
 </x-layout>
