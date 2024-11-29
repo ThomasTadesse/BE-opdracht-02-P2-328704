30df38702f9d76bfbc5599661154feb8618aa4c7
ThomasTadesse <328704@student.mboutrecht.nl>
2024-11-29 11:35:39 +0100

    Basic MVC compleet. Styling TODO

--- magazijn_jamin/routes/web.php
+++ magazijn_jamin/routes/web.php
@@ -9,11 +9,11 @@ Route::view('/', 'welcome'); // view welcome.blade.php
 
 Route::resource('magazijn', MagazijnController::class);
 
-Route::get('/magazijn', [MagazijnController::class, 'index']); // view magazijn.blade.php
-Route::get('/magazijn/{id}', [MagazijnController::class, 'show']); // view magazijn.blade.php
-Route::get('/magazijn/create', [MagazijnController::class, 'create']); // view create.blade.php
-Route::get('/magazijn/{id}/edit', [MagazijnController::class, 'edit']); // view edit.blade.php
-Route::post('/magazijn', [MagazijnController::class, 'store']); // store data
-Route::put('/magazijn/{id}', [MagazijnController::class, 'update']); // update data
-Route::delete('/magazijn/{id}', [MagazijnController::class, 'destroy']); // delete data
+Route::get('/magazijn', [MagazijnController::class, 'index'])->name('magazijn.index'); // view magazijn.blade.php
+Route::get('/magazijn/{id}', [MagazijnController::class, 'show'])->name('magazijn.show'); // view magazijn.blade.php
+Route::get('/magazijn/create', [MagazijnController::class, 'create'])->name('magazijn.create'); // view create.blade.php
+Route::get('/magazijn/{id}/edit', [MagazijnController::class, 'edit'])->name('magazijn.edit'); // view edit.blade.php
+Route::post('/magazijn', [MagazijnController::class, 'store'])->name('magazijn.store'); // store data
+Route::put('/magazijn/{id}', [MagazijnController::class, 'update'])->name('magazijn.update'); // update data
+Route::delete('/magazijn/{id}', [MagazijnController::class, 'destroy'])->name('magazijn.destroy'); // delete data
 
