33ba3470bcff35901f930823aafd73144ec2c158
ThomasTadesse <328704@student.mboutrecht.nl>
2024-12-03 11:24:25 +0100

    edit/update pending

--- magazijn_jamin/resources/views/Leverancier/edit.blade.php
+++ magazijn_jamin/resources/views/Leverancier/edit.blade.php
@@ -1,28 +1,22 @@
 <x-layout>
 <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-4">Bewerk Leverancier</h1>
+    <h1 class="text-2xl font-bold mb-4">Levering Product</h1>
+    <div class="mb-4">
+        <p><strong>Naam leverancier:</strong> {{ $leverancier->Naam }}</p>
+        <p><strong>Contactpersoon:</strong> {{ $leverancier->Contactpersoon }}</p>
+        <p><strong>Leverancier NR:</strong> {{ $leverancier->Leveranciernummer }}</p>
+        <p><strong>Mobiel:</strong> {{ $leverancier->Mobiel }}</p>
+    </div>
     <form action="{{ route('leverancier.update', $leverancier->id) }}" method="POST">
         @csrf
         @method('PUT')
-        <div class="mb-3">
-            <label for="naam" class="form-label">Naam</label>
-            <input type="text" class="form-control" id="naam" name="naam" value="{{ $leverancier->naam }}" required>
+        
+        
+        <button type="submit" class="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Sla Op</button>
+        <div class="mt-6 flex justify-end">
+            <a href="{{ route('leverancier.index') }}" class="mb-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Terug</a>
+            <a href="{{ route('welcome') }}" class="mb-4 inline-block px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Home</a>
         </div>
-        <div class="mb-3">
-            <label for="contactpersoon" class="form-label">Contactpersoon</label>
-            <input type="text" class="form-control" id="contactpersoon" name="contactpersoon" value="{{ $leverancier->contactpersoon }}" required>
-        </div>
-        <div class="mb-3">
-            <label for="leveranciernummer" class="form-label">Leveranciernummer</label>
-            <input type="text" class="form-control" id="leveranciernummer" name="leveranciernummer" value="{{ $leverancier->leveranciernummer }}" required>
-        </div>
-        <div class="mb-3">
-            <label for="mobiel" class="form-label">Mobiel</label>
-            <input type="text" class="form-control" id="mobiel" name="mobiel" value="{{ $leverancier->mobiel }}" required>
-        </div>
-        <button type="submit" class="mb-4 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Sla op</button>
-        <a href="{{ route('leverancier.index') }}" class="mb-4 inline-block px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Terug</a>
-        <a href="{{ route('home') }}" class="mb-4 inline-block px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Home</a>
     </form>
 </div>
 </x-layout>
